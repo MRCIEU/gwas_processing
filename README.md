b2711b3a549a5e6399635470a121fd7cecd429f3
Gibran Hemani <explodecomputer@gmail.com>
2019-02-05 00:06:39 +0000

    updated

---
+++ README.md
@@ -0,0 +1,36 @@
+# Clumping GWAS files and performing LDSC
+
+## Setup
+
+```
+git clone https://github.com/bulik/ldsc.git
+cd ldsc
+mkdir ref
+cd ref
+wget https://data.broadinstitute.org/alkesgroup/LDSCORE/eur_w_ld_chr.tar.bz2
+bunzip2 eur_w_ld_chr.tar.bz2
+tar xvf eur_w_ld_chr.tar
+zcat eur_w_ld_chr/*gz | awk '{ print $1"\t"$3 }' | grep -v "CHR" > vars.txt
+wget -O ld_files.tgz https://www.dropbox.com/s/yuo7htp80hizigy/ld_files.tgz?dl=0
+tar xzvf ld_files.tgz 
+rm ld_files.tgz
+rm eur_w_ld_chr.tar
+rm ld_files/data_maf0.01.*
+conda env create -f ldsc/environment.yml
+source activate ldsc
+```
+
+To run clumping
+
+```{r}
+./clump.py --bcf <BCF> --plink_ref ref/ld_files/data_maf0.01_rs --out <OUTPUT>
+```
+
+To run LDSC
+
+```{r}
+bcftools query -R ref/vars.txt -f'%ID %EFFECT %SE %N\n' <BCF> | awk 'BEGIN {print "SNP Z N"}; { print $1, $2/$3, $4 }' > <BCF>.temp
+
+ldsc/ldsc.py --h2 <BCF>.temp --ref-ld-chr ../ref/eur_w_ld_chr/ --w-ld-chr ../ref/eur_w_ld_chr/ --out <OUT>
+```
+
---
+++ clump.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import argparse
+import math
+import os
+
+parser = argparse.ArgumentParser(description = 'Extract and clump top hits')
+parser.add_argument('--bcf', required=True)
+parser.add_argument('--bcftools_binary', default='bcftools')
+parser.add_argument('--plink_binary', default='plink')
+parser.add_argument('--plink_ref', required=True)
+parser.add_argument('--clump_pval', type=float, default=5e-8)
+parser.add_argument('--clump_kb', type=int, default=10000)
+parser.add_argument('--clump_r2', type=float, default=0.001)
+parser.add_argument('--out', required=True)
+args = parser.parse_args()
+
+# args = parser.parse_args(['--bcf', 'data.bcf', '--plink_ref', '../../vcf-reference-datasets/1000g_filtered/data_maf0.01_rs', '--out', 'temp'])
+
+cmd = "{0} view -i 'L10PVAL>{1}' {2} | bcftools query -f'%ID %L10PVAL\n' | awk 'BEGIN {{print \"SNP P\"}}; {{print $1, 10^-$2}}' > {3}.tophits".format(args.bcftools_binary, -math.log10(args.clump_pval), args.bcf, args.out)
+os.system(cmd)
+
+cmd = "{0} --bfile {1} --clump {2} --clump-kb {3} --clump-r2 {4} --clump-p1 {5} --clump-p2 {5} --out {2}".format(args.plink_binary, args.plink_ref, args.out+".tophits", args.clump_kb, args.clump_r2, args.clump_pval)
+os.system(cmd)
+
+if os.path.isfile(args.out+'.tophits.clumped'):
+	n=0
+	with open(args.out+'.tophits.clumped', 'rt') as fp, open(args.out, 'wt') as fo:
+		o=next(fp)
+		for line in fp:
+			line = line.strip("\n").split()
+			if len(line) >= 3:
+				n+=1
+				fo.write("{0}\n".format(line[2]))
+	print("Found "+str(n)+" hits")
+else:
+	subprocess.call(['touch', args.out])
+	print("No hits")
+
+[os.remove(args.out + x) for x in ['.tophits.clumped', '.tophits.log', '.tophits.nosex', '.tophits'] if os.path.isfile(args.out + x)]
