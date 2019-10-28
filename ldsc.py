39574c773d274f1ff71c334919a1725c69560fdc
Gibran Hemani <explodecomputer@gmail.com>
2019-10-28 21:56:05 +0000

    fixing ldsc

--- ldsc.py
+++ ldsc.py
@@ -9,15 +9,14 @@ parser = argparse.ArgumentParser(description="Wrapper for LD score regression")
 parser.add_argument("--bcf", required=True)
 parser.add_argument("--ldsc_repo", default="./ldsc")
 parser.add_argument("--ldsc_ref", default="/data/ref/eur_w_ld_chr/")
-parser.add_argument("--snplist", default="/data/ref/snplist.gz")
 parser.add_argument("--out", required=True)
 args = parser.parse_args()
 
 
 cmd = " ".join(
     [
-        "{0}/ldsc.py --h2 {1} --snplist {2} --ref-ld-chr {3}",
-        " --w-ld-chr {3} --out {4}",
+        "{0}/ldsc.py --h2 {1} --ref-ld-chr {2}",
+        " --w-ld-chr {2} --out {3}",
     ]
-).format(args.ldsc_repo, args.bcf, args.snplist, args.ldsc_ref, args.out)
+).format(args.ldsc_repo, args.bcf, args.ldsc_ref, args.out)
 os.system(cmd)
